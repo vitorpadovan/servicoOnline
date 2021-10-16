55eff26c6c1880f4dace8b9f6735d0b74b137f11
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2021-10-16 11:34:50 -0300

    Commit inicial

---
+++ src/test/java/com/br/vitorhernandes/servicoOnline/ServicoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.vitorhernandes.servicoOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
